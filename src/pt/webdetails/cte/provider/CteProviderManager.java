e672aad209983e8125b4dbadbd665a9253a3160a
Pedro Teixeira <pedrofvteixeira@gmail.com>
2015-02-19 14:56:34 +0000

    added addProvider() and ProviderExists() methods to ProviderManager

--- src/pt/webdetails/cte/provider/CteProviderManager.java
+++ src/pt/webdetails/cte/provider/CteProviderManager.java
@@ -13,6 +13,8 @@
 package pt.webdetails.cte.provider;
 
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pt.webdetails.cpf.exceptions.InitializationException;
 import pt.webdetails.cte.api.ICteProvider;
 
@@ -20,6 +22,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CteProviderManager {
+
+  private Logger logger = LoggerFactory.getLogger( CteProviderManager.class );
 
   List<ICteProvider> providers = new ArrayList<ICteProvider>();
 
@@ -31,24 +35,16 @@ public class CteProviderManager {
 
     // validate all provider id's are unique
 
-    List<String> ids = new ArrayList<String>();
-
     for ( ICteProvider provider : providers ) {
 
-      if ( provider != null ) {
+      if ( provider == null || StringUtils.isEmpty( provider.getId() ) ) {
+        throw new InitializationException( "ICteProvider " + provider.getClass().getSimpleName() + " with null ID. "
+            + "Each provider must have a unique ID", null );
+      }
 
-        if ( StringUtils.isEmpty( provider.getId() ) ) {
-          throw new InitializationException( "ICteProvider " + provider.getClass().getSimpleName() + " with null ID. "
-              + "Each provider must have a unique ID", null );
-        }
-
-        if ( ids.contains( provider.getId() ) ) {
-          throw new InitializationException( "Duplicated ICteProvider id: " + provider.getId() +
-              ". Each provider must have a unique ID", null );
-
-        } else {
-          ids.add( provider.getId() );
-        }
+      if ( providerExists( provider ) ) {
+        throw new InitializationException( "Duplicated ICteProvider id: " + provider.getId() +
+            ". Each provider must have a unique ID", null );
       }
     }
 
@@ -62,7 +58,7 @@ public class CteProviderManager {
 
   public ICteProvider getProviderById( String id ) {
 
-    if ( !StringUtils.isEmpty( id ) ) {
+    if ( providerExists( id ) ) {
 
       for ( ICteProvider provider : providers ) {
 
@@ -79,4 +75,36 @@ public class CteProviderManager {
     this.providers = providers;
   }
 
+  public boolean addProvider( ICteProvider provider, boolean override ) {
+
+    if ( provider == null || StringUtils.isEmpty( provider.getId() ) ) {
+      logger.error( "Cannot add a provider with an empty ID" );
+      return false;
+
+    } else if ( providerExists( provider ) && !override ) {
+      logger.warn( "Provider already exists and override set to false" );
+      return false;
+    }
+
+    getProviders().add( provider );
+    return true;
+  }
+
+  public boolean providerExists( ICteProvider provider ) {
+    return provider != null && providerExists( provider.getId() );
+  }
+
+  public boolean providerExists( String id ) {
+
+    boolean matchFound = false;
+
+    if ( !StringUtils.isEmpty( id ) ) {
+
+      for ( ICteProvider provider : getProviders() ) {
+        matchFound |= id.equals( provider.getId() );
+      }
+    }
+
+    return matchFound;
+  }
 }
